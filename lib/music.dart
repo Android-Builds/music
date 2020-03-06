c4e0feec58cd0514569501138ab0a6fda1f415ff
xayron <zxyron0@gmail.com>
2020-03-06 23:07:51 +0530

    Remove Album page for now

--- lib/music.dart
+++ lib/music.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
       child: Playlists(),
     ),
     Container(
-      child: Album(songModel: songmodel),
+      // child: Album(songModel: songmodel),
     )
   ];
 
